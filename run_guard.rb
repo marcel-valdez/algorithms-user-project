d0abd381410dd7863d0b79c3b879544e1d1e882a
Marcel Valdez <marcel.valdez@live.com>
2013-04-21 03:59:28 -0500

    Update run_guard.rb

--- run_guard.rb
+++ run_guard.rb
@@ -1,7 +1,3 @@
-kill_spork_manually = <<BASH
-ps -ax | grep -i spork | grep -i -v  "grep.-i.spork" | awk '{print $1}' | xargs kill
-BASH
-
 kill_all_guard = <<BASH
 ps -ax | grep -i bin/guard | grep -i -v "grep.*-i.*bin/guard" | awk '{print $1}' | xargs kill
 BASH
